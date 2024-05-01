ab343cce7a8765eded4fd7db4252f4da17d85cdc
Roddy <roddymcg787@gmail.com>
2024-04-30 21:38:30 -0700

    first commit

---
+++ main_redone.py
@@ -0,0 +1,19 @@
+import random
+import customtkinter as ctk
+
+ctk.set_default_color_theme('dark-blue')
+ctk.set_appearance_mode('dark')
+Root = ctk.CTk()
+Root.geometry("700x130")
+Root.title("Random number generator")
+my_font = ctk.CTkFont(family="", size=40)
+# main_frame = ctk.CTkFrame(master=Root, width=500, height=500).pack(fill="both",pady=10,padx=10)
+u=0
+course_label = ctk.CTkLabel(master=Root, text="random numbers", font=my_font)
+course_label.pack(pady=12,padx=10)
+def increment():
+    randomlist = random.sample(range(1, 100), 10)
+    course_label.configure(text = randomlist)
+button1= ctk.CTkButton(master=Root, text="generate", command=increment).pack(pady=12,padx=10)
+
+Root.mainloop()
---
+++ mainoptimized.py
@@ -0,0 +1,46 @@
+import pygame
+import random
+"""
+this is the 'optimized' version of my first program, honestly i have no idea how 
+it was so optimized to begin with seing as i dint know the laungauge and i
+smashed a pygame and a randomization tutioral together to make this.
+"""
+WIDTH, HEIGHT = 900, 900
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("number genorator")
+
+pygame.font.init()
+Health_font = pygame.font.SysFont('comicsans', 40)
+number_Font = pygame.font.SysFont('comicsans', 40)
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+FPS = 60
+DELAY = 900
+
+
+def draw_window():
+    draw_number()
+    WIN.fill(WHITE)
+
+def draw_number():
+    randomlist = random.sample(range(1, 100), 10)
+    draw_text = number_Font.render(str(randomlist), 1, BLACK)
+    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width()/2, HEIGHT/2 - draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.display.update()
+    pygame.time.delay(DELAY)
+
+def main():
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(FPS)
+        draw_window()
+        if pygame.event.get(pygame.QUIT):
+            pygame.quit()
+
+
+
+
+if __name__ == "__main__":
+    main()
