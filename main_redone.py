fd946fb14890a7837ca7783ae2d48449a4945685
Roddy <roddymcg787@gmail.com>
2024-04-30 21:52:19 -0700

    fixed formating

--- main_redone.py
+++ main_redone.py
@@ -1,5 +1,6 @@
 import random
 import customtkinter as ctk
+
 def main():
     ctk.set_default_color_theme('dark-blue')
     ctk.set_appearance_mode('dark')
@@ -7,15 +8,17 @@ def main():
     Root.geometry("700x130")
     Root.title("Random number generator")
     my_font = ctk.CTkFont(family="", size=40)
-    # main_frame = ctk.CTkFrame(master=Root, width=500, height=500).pack(fill="both",pady=10,padx=10)
-    u=0
-    course_label = ctk.CTkLabel(master=Root, text="random numbers", font=my_font)
-    course_label.pack(pady=12,padx=10)
-    def increment():
-        randomlist = random.sample(range(1, 100), 10)
-        course_label.configure(text = randomlist)
-    button1= ctk.CTkButton(master=Root, text="generate", command=increment).pack(pady=12,padx=10)
 
+    def module_setup():
+        course_label = ctk.CTkLabel(master=Root, text="random numbers", font=my_font)
+        course_label.pack(pady=12,padx=10)
+        
+        def increment():
+            randomlist = random.sample(range(1, 100), 10)
+            course_label.configure(text = randomlist)
+
+        button1= ctk.CTkButton(master=Root, text="generate", command=increment).pack(pady=12,padx=10)
+    module_setup()
     Root.mainloop()
 if __name__ == "__main__":
     main()
